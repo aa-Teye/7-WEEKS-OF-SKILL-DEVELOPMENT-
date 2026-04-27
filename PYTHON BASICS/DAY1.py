3351c43010e3920f16a5fa7a36921d0e06c2684e
aa-Teye <alexteyeametepey@gmail.com>
2026-04-27 19:28:49 +0000

    Working on the basic of python as revision process for the entire course

--- PYTHON BASICS/DAY1.py
+++ PYTHON BASICS/DAY1.py
@@ -1,4 +1,14 @@
-print("hello world")
-name = "hello Mr. Alex "
-print(name)
-print(f"name is {name}")
+# print("hello world")
+# name = "hello Mr. Alex "
+# print(name)
+# print(f"name is {name}")
+
+# name =  input("what is your name? ")
+# print(f"hello my name is {name}")
+print("\n --- Welcome to The Voting Eligibility Checker ---")
+name = input("what is your name? ")
+age = int(input ("Please enter your age : "))
+if age >= 18 :
+    print("you are eligible to vote")
+else: 
+    print("You are ")
