1a600061ccc1419d9601a339153991b2d6de8e6f
aa-Teye <alexteyeametepey@gmail.com>
2026-04-28 23:27:19 +0000

    Adding finishing tactics

--- PYTHON BASICS/DAY1.py
+++ PYTHON BASICS/DAY1.py
@@ -5,10 +5,12 @@
 
 # name =  input("what is your name? ")
 # print(f"hello my name is {name}")
+
+
 print("\n --- Welcome to The Voting Eligibility Checker ---")
 name = input("what is your name? ")
 age = int(input ("Please enter your age : "))
 if age >= 18 :
     print("you are eligible to vote")
 else: 
-    print("You are ")
+    print("You are not elgible to vote") 
