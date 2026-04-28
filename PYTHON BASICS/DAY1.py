4934fd3fb0b49126939c33fd72851cfc57338655
aa-Teye <alexteyeametepey@gmail.com>
2026-04-28 23:48:20 +0000

    Adding the loops to the basics

--- PYTHON BASICS/DAY1.py
+++ PYTHON BASICS/DAY1.py
@@ -7,10 +7,19 @@
 # print(f"hello my name is {name}")
 
 
-print("\n --- Welcome to The Voting Eligibility Checker ---")
-name = input("what is your name? ")
-age = int(input ("Please enter your age : "))
-if age >= 18 :
-    print("you are eligible to vote")
-else: 
-    print("You are not elgible to vote") 
+# print("\n --- Welcome to The Voting Eligibility Checker ---")
+# name = input("what is your name? ")
+# age = int(input ("Please enter your age : "))
+# if age >= 18 :
+#     print("you are eligible to vote")
+# else: 
+#     print("You are not elgible to vote") 
+
+count = 5
+while count < 10 :
+    print(count)
+    count = count + 1
+
+for num in range (10) :
+    print(f"number is now  {num}")
+    
