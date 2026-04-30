84bd7d6d9d86aca622800cf699fd719ba34fd9da
aa-Teye <alexteyeametepey@gmail.com>
2026-04-30 09:57:22 +0000

    Initializing Day and Updating Day1 Project

--- PYTHON BASICS/DAY1.py
+++ PYTHON BASICS/DAY1.py
@@ -1,12 +1,28 @@
-# WORKING ON THE PRINT GENERATOR 
-print("hello world")
-print("Welcome to Polling Stations")
+# # WORKING ON THE PRINT GENERATOR 
+# print("hello world")
+# print("Welcome to Polling Stations")
 
-print("\n ----Welcome to The Message Generator Space---")
+# print("\n ----Welcome to The Message Generator Space---")
 
-#PRINTING NUMBERS 
-print( 3 + 4 )
+# #PRINTING NUMBERS 
+# print( 3 + 4 )
+# print(10 - 2)
 
-#PRINTING DYNAMIC MESSAGES 
-name = "Livingston"
-print(f"Welcome to 7 Weeks of Python Programming {name}! It is going to be an amazing journey all together")
+# #PRINTING DYNAMIC MESSAGES 
+# name = "Livingston"
+# print(f"Welcome to 7 Weeks of Python Programming {name}! It is going to be an amazing journey all together")
+
+
+# username = input("What is your name?")
+# print(f"Welcome to 7 Weeks of Python Programming {username}! It is going to be an amazing journey all together")
+
+
+# DAY1 PROJECT 
+print("\n ----Welcome to Message Geneator----")
+#STEP1 ASK THE USER FOR THEIR DETAILS
+name = input("What is your name? ")
+hobby = input("What is your hobby? ")
+
+#STEP2 GENERATE A PERSONALIZED MESSAGE
+print(f"Hello {name}!Welcome to University of Ghana.We are excited to  have you here. We were informed your favorite hobby is {hobby}, You are not alone there are more people interesed in {hobby}")
+print("All the best in your studies and we hope you have a great time here at University of Ghana.")
