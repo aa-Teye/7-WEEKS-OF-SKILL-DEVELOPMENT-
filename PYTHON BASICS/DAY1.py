c5244546f277f8d72a7e388cada2cbc7d5b80e44
aa-Teye <alexteyeametepey@gmail.com>
2026-04-30 09:37:16 +0000

    Initializing Day1 of the Basics course

---
+++ PYTHON BASICS/DAY1.py
@@ -0,0 +1,12 @@
+# WORKING ON THE PRINT GENERATOR 
+print("hello world")
+print("Welcome to Polling Stations")
+
+print("\n ----Welcome to The Message Generator Space---")
+
+#PRINTING NUMBERS 
+print( 3 + 4 )
+
+#PRINTING DYNAMIC MESSAGES 
+name = "Livingston"
+print(f"Welcome to 7 Weeks of Python Programming {name}! It is going to be an amazing journey all together")
